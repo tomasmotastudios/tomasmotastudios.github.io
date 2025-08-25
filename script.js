0744f49d139a2cd5d55fadb6d18d7ca8736dd53a
Tomás Mota <tomasmotadev+github@proton.me>
2025-08-25 12:11:30 +0000

    docs: script.js
    - Add the JavaScript code to change the theme.

--- script.js
+++ script.js
@@ -85,3 +85,25 @@ document.addEventListener('DOMContentLoaded', () => {
         setInterval(showNextSlide, 3000);
     });
 });
+
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+// Carregar tema salvo no localStorage
+if (localStorage.getItem("theme") === "light") {
+  body.classList.add("light-mode");
+  toggleBtn.textContent = "☀️";
+}
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("light-mode");
+
+  if (body.classList.contains("light-mode")) {
+    localStorage.setItem("theme", "light");
+    toggleBtn.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.textContent = "🌙";
+  }
+});
+
