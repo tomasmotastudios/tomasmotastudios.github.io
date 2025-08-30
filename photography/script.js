eea3effb32c21c579379f5ca9621bc09b25b2dd1
Tomás Mota <tomasmotadev+github@proton.me>
2025-08-30 19:29:30 +0000

    docs: /photography/script.js

--- photography/script.js
+++ photography/script.js
@@ -12,31 +12,30 @@ menuBtn.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
 });
 
-// Dropdown Photography
-const mobilePhotoBtn = document.getElementById('mobile-photography-btn');
-const mobilePhotoMenu = document.getElementById('mobile-photography-menu');
+// Slideshow @ Photography
+document.addEventListener('DOMContentLoaded', () => {
+    const slideshowContainers = document.querySelectorAll('.slideshow-container');
+    slideshowContainers.forEach(container => {
+        const slides = container.querySelectorAll('.slide');
+        if (slides.length < 2) return; 
 
-mobilePhotoMenu.style.display = 'none';
-mobilePhotoBtn.classList.remove('active');
+        let current = 0;
 
-mobilePhotoBtn.addEventListener('click', () => {
-  const isOpen = mobilePhotoMenu.style.display === 'block';
-  mobilePhotoMenu.style.display = isOpen ? 'none' : 'block';
-  mobilePhotoBtn.classList.toggle('active', !isOpen);
+        function showNextSlide() {
+            slides[current].classList.remove('active');
+            current = (current + 1) % slides.length;
+            slides[current].classList.add('active');
+        }
+
+        // Garante que a primeira imagem está ativa ao carregar
+        slides[0].classList.add('active');
+        
+        setInterval(showNextSlide, 3000);
+    });
 });
 
-// Dropdown Contact
-const mobileContactBtn = document.getElementById('mobile-contact-btn');
-const mobileContactMenu = document.getElementById('mobile-contact-menu');
-
-mobileContactMenu.style.display = 'none';
-mobileContactBtn.classList.remove('active');
-
-mobileContactBtn.addEventListener('click', () => {
-  const isOpen = mobileContactMenu.style.display === 'block';
-  mobileContactMenu.style.display = isOpen ? 'none' : 'block';
-  mobileContactBtn.classList.toggle('active', !isOpen);
-});
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
 
 
 
