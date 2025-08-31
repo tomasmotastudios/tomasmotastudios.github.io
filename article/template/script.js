115109e33115f2b865b0fe814f65bfe8d473ce6e
Tomás Mota <tomasmotadev+github@proton.me>
2025-08-31 18:14:43 +0000

    docs: /article/template/script.js

---
+++ article/template/script.js
@@ -0,0 +1,44 @@
+// Ano do footer
+document.getElementById('year').textContent = new Date().getFullYear();
+
+//
+// Mobile Menu
+//
+// Menu mobile toggle
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+});
+
+// Dropdown Photography
+const mobilePhotoBtn = document.getElementById('mobile-photography-btn');
+const mobilePhotoMenu = document.getElementById('mobile-photography-menu');
+
+mobilePhotoMenu.style.display = 'none';
+mobilePhotoBtn.classList.remove('active');
+
+mobilePhotoBtn.addEventListener('click', () => {
+  const isOpen = mobilePhotoMenu.style.display === 'block';
+  mobilePhotoMenu.style.display = isOpen ? 'none' : 'block';
+  mobilePhotoBtn.classList.toggle('active', !isOpen);
+});
+
+const toggleBtn = document.getElementById("theme-toggle");
+const body = document.body;
+
+
+
+    // Calcular tempo de leitura
+    function calculateReadingTime() {
+      const article = document.querySelector("article");
+      const text = article.innerText;
+      const words = text.trim().split(/\s+/).length;
+      const wordsPerMinute = 200;
+      const minutes = Math.ceil(words / wordsPerMinute);
+
+      document.getElementById("reading-time").textContent = `${minutes} min read`;
+    }
+
+    calculateReadingTime();
